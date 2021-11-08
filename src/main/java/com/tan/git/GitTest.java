f7423c9f8e71725b4e02bb478445b59ed8cce8ef
tanjezh <tan19972100@163.com>
2021-11-08 22:13:11 +0800

    second commit

--- src/main/java/com/tan/git/GitTest.java
+++ src/main/java/com/tan/git/GitTest.java
@@ -7,5 +7,6 @@ package com.tan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
