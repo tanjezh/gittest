c98a6b372815a9c273735ec72f103f6ca4c55e2d
tanjezh <tan19972100@163.com>
2021-11-08 22:10:48 +0800

    first commit

---
+++ src/main/java/com/tan/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.tan.git;
+
+/**
+ * @author tanjezh
+ * @create 2021-11-08 21:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
